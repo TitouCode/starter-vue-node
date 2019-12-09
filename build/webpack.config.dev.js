8ead9421563e2208a45d7d515e188a216e550c2e
Titouan <titouan@macbook-pro-de-titouan.home>
2019-12-09 16:20:06 +0100

    Init Project Vue Side

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,84 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+
+const PUBLIC_PATH = path.resolve(__dirname, '../public')
+
+// GET VAR FROM config.json
+const ASSETS_PATH_VAR = 'src/assets/'
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        './src/app.js'
+    ],
+    output: {
+        filename: 'app.js',
+        path: path.resolve(PUBLIC_PATH, 'js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                    // outputPath: '../assets/',
+                    // publicPath: 'public/assets/',
+                    // PATH FOR ASSETS IN SCSS
+                    outputPath: (url) => {
+                        const re = new RegExp(ASSETS_PATH_VAR);
+
+                        if (re.test(url)) {
+                            return `../assets/${url.replace(re, '')}`;
+                        }
+            
+                        return '../assets/';
+                    },
+                    publicPath: (url) => {
+                        const re = new RegExp(ASSETS_PATH_VAR);
+
+                        if (re.test(url)) {
+                            return `public/assets/${url.replace(re, '')}`;
+                        }
+            
+                        return 'public/assets/';
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        // Copy Assets to Public Dir
+        new CopyWebpackPlugin(
+            [{
+                from: path.resolve(__dirname, '../src/assets'),
+                to: path.resolve(__dirname, '../public/assets')
+            }]
+        ),
+    ],
+    resolve: {
+        // Aliases to root components and assets
+        alias: {
+            assets: path.resolve(__dirname, '../src/assets'),
+        }
+    } 
+}
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="clickMe" @click="clickMe">Click Me</div>
+    <div v-if="displayOrange">
+        <h1 class="title" >{{ cha }}</h1>
+        <img src="public/assets/cute_img/cute.jpg" width=50 height=50>
+        <div class="orange" />
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data: () => {
+            return {
+                cha: 'Coucou c\'est  orange !!!',
+                displayOrange: false
+            }   
+        },
+        methods: {
+            clickMe: function() {
+                return (this.displayOrange = !this.displayOrange);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .title {
+        color: orange;
+        font: 24px/24px 'Arial', sans-serif;
+    }
+    .clickMe {
+        &:hover {
+            color: orange;
+            cursor: pointer;
+        }
+    }
+    .orange {
+        /*TODO: change path to img*/
+        // background: url('assets/cute.jpg') center no-repeat;
+        background: url('~assets/cute_img/cute.jpg') center no-repeat;
+        background-size: contain;
+        height: 50px;
+        width: 50px;
+    }
+</style>
